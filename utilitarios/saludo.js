56ce5d62ce7e00bc48ff9a5488e59cf7a510f73a
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-01-10 20:58:06 -0500

    Funciones recuperarInt y recuperarFloat

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -3,6 +3,11 @@ saludar=function(){
     let nombre=recuperarTexto("txtNombre");
     //recupera el valor de la caja de texto apellido
     let apellido=recuperarTexto("txtApellido");
+    //recupera el valor de la caja de texto edad
+    let edad=recuperarInt("txtEdad");
+    //recupera el valor de la caja de texto estatura
+    let estatura=recuperarFloat("txtEstatura");
+
 }
 
 recuperarTexto=function(idComponente){
@@ -11,4 +16,14 @@ recuperarTexto=function(idComponente){
     componente=document.getElementById(idComponente);
     valorIngresado=componente.value;
     return valorIngresado
+}
+
+recuperarInt=function(idComponente){
+    let valorInt=parseInt(recuperarTexto(idComponente));
+    return valorInt
+}
+
+recuperarFloat=function(idComponente){
+    let valorFloat=parseFloat(recuperarTexto(idComponente));
+    return valorFloat
 }
