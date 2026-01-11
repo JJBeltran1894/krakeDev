c2c066998bcb3edb9dc987864a30851e875ac4c4
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-01-10 21:46:58 -0500

    Se separan las funciones en utilitarios.js

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,3 +1,4 @@
+
 saludar=function(){
     //recupera el valor de la caja de texto nombre
     let nombre=recuperarTexto("txtNombre");
@@ -13,35 +14,3 @@ saludar=function(){
     mostrarTextoEnCaja("txtNombre","")
 }
 
-recuperarTexto=function(idComponente){
-    let componente;
-    let valorIngresado;
-    componente=document.getElementById(idComponente);
-    valorIngresado=componente.value;
-    return valorIngresado
-}
-
-recuperarInt=function(idComponente){
-    let valorInt=parseInt(recuperarTexto(idComponente));
-    return valorInt
-}
-
-recuperarFloat=function(idComponente){
-    let valorFloat=parseFloat(recuperarTexto(idComponente));
-    return valorFloat
-}
-
-mostrarTexto=function(idComponente,mensaje){
-    let componente=document.getElementById(idComponente);
-    componente.innerText=mensaje;
-}
-
-mostrarImagen=function(idComponente,rutaImagen){
-    let componente=document.getElementById(idComponente);
-    componente.src=rutaImagen;
-}
-
-mostrarTextoEnCaja=function(idComponente,mensaje){
-    let componente=document.getElementById(idComponente);
-    componente.value=mensaje;
-}
