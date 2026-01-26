c7172c8b04e45b7b6e56f398f1c9883e21ba0948
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-01-26 01:44:56 -0500

    Reto 36 - carga inicial - se agregan funciones en servicioJuego.js

---
+++ juego/serviciosJuego.js
@@ -0,0 +1,42 @@
+obtenerAleatorio=function(){
+    numero=parseInt(Math.random()*3)+1;
+    console.log(numero);
+    return numero;
+}
+
+generarElemento=function(){
+    let numero=obtenerAleatorio();
+    let elemento;
+    if(numero==1){
+        elemento="piedra";
+    }else if(numero==2){
+        elemento="papel";
+    }else if(numero==3){
+        elemento="tijera";
+    }
+}
+
+determinarGanador=function(eleccionJuagdor1,eleccionJuagdor2){
+    let ganador;
+    if(eleccionJuagdor1==eleccionJuagdor2){
+        ganador=0;
+    }else if(eleccionJuagdor1=="piedra" & eleccionJuagdor2=="papel"){
+        ganador=2;
+    }else if(eleccionJuagdor1=="piedra"& eleccionJuagdor2=="tijera"){
+        ganador=1;
+    }else if(eleccionJuagdor1=="papel"& eleccionJuagdor2=="piedra"){
+        ganador=1;
+    }else if(eleccionJuagdor1=="papel"& eleccionJuagdor2=="tijera"){
+        ganador=2;
+    }else if(eleccionJuagdor1=="tijera"& eleccionJuagdor2=="piedra"){
+        ganador=2;
+    }else if(eleccionJuagdor1=="tijera"& eleccionJuagdor2=="papel"){
+        ganador=1;
+    }
+    return ganador;
+}
+
+generarRuta=function(seleccion){
+    let ruta="./imagenes/"+seleccion+".png";
+    return ruta;
+}
