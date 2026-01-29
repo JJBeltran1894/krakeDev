54a644bf03a043149d761022c0203a3e80282969
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-01-28 20:13:11 -0500

    Reto 40 - Completado Paso 1

--- juegoAhorcado/ahorcado.js
+++ juegoAhorcado/ahorcado.js
@@ -1,1 +1,12 @@
-//No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+//Paso 1
+
+esMayuscula=function(caracter){
+    let codigo=caracter.charCodeAt();
+    let esMayusc;
+    if(codigo>=65 && codigo<=90){
+        esMayusc=true;
+    }else{
+        esMayusc=false;
+    }
+    return esMayusc;
+}
