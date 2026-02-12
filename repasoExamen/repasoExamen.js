c30c96eed463c120f5f226bfe2e10af8607d67d5
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-02-11 21:34:09 -0500

    Subida repaso examen 1

---
+++ repasoExamen/repasoExamen.js
@@ -0,0 +1,53 @@
+ejecutar1=function(){
+    for(let j=0;j<=5;j++){
+        console.log(j);
+    }
+}
+
+ejecutar2=function(){
+    for(let j=10;j<=20;j++){
+        console.log(j);
+    }
+}
+
+ejecutar3=function(){
+    for(let j=10;j<20;j++){
+        console.log(j);
+    }
+}
+
+ejecutar4=function(){
+    for(let j=20;j>=10;j--){
+        console.log(j);
+    }
+}
+
+ejecutar5=function(){
+    for(let j=0;j<=20;j+=2){
+        console.log(j);
+    }
+}
+
+ejecutar6=function(){
+    for(let j=1;j<=10;j+=2){
+        console.log(j);
+    }
+}
+
+ejecutar7=function(){
+    for(let j=2;j<=10;j+=2){
+        console.log(j);
+    }
+}
+
+ejecutar8=function(){
+    for(let j=2;j<=10;j+=2){
+        console.log(j);
+    }
+}
+
+ejecutar9=function(){
+    for(let j=0;j<10;j+=2){
+        console.log(j);
+    }
+}
